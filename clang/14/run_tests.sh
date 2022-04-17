67deb6d3a8fa7b95a22a4ecce5781020b0b231c1
nakat-t <armaiti.wizard@gmail.com>
2022-04-17 22:21:45 +0900

    Add clang-12, 13, 14

---
+++ clang/14/run_tests.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -eu
+
+version=14
+
+for cmd in clang clang-${version} clang++ clang++-${version} clang-format clang-format-${version} clang-tidy clang-tidy-${version} llvm-ar llvm-ar-${version}
+do
+  result=0
+  output1=$(${cmd} --version 2>&1) || result=$?
+  if [ ! "${result}" = "0" ]; then
+    echo >&2 "ERROR: command '${cmd}' not found."
+    exit ${result}
+  fi
+  output2=$(echo ${output1} | grep "version ${version}") || result=$?
+  if [ ! "${result}" = "0" ]; then
+    echo >&2 "ERROR: wrong version of command '${cmd}'."
+    exit ${result}
+  fi
+done
